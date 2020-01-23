b49578b02659a3fe5a7eed0714fee1c92b305768
Fabio Dias <mfabiodias@gmail.com>
2020-01-23 13:13:28 -0300

    Componente MineField adicionado

--- App.js
+++ App.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View,} from 'react-native'
 import params from './src/params'
-import Field from './src/components/Field'
+import MineField from './src/components/MineField'
+import { createMinedBoard } from './src/functions'
 
 export default class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = this.createState()
+  }
+  
+  minesAmount = () => {
+    const rows = params.getRowsAmount()
+    const cols = params.getColumnsAmount()
+
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const rows = params.getRowsAmount()
+    const cols = params.getColumnsAmount()
+
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+  }
+
+
   render() {
     return(
       <View style={styles.container}>
         <Text style={styles.welcome}>Iniciando o Mines</Text>
-        <Text style={styles.welcome}>
-          Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
-        </Text>
-
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1}/>
-        <Field opened nearMines={2}/>
-        <Field opened nearMines={3}/>
-        <Field opened nearMines={4}/>
-        <Field opened nearMines={5}/>
-        <Field opened nearMines={6}/>
-        <Field opened nearMines={7}/>
-        <Field mined />
-        <Field opened mined />
-        <Field opened mined exploded />
-        <Field flagged />
-        <Field flagged opened />
+        <Text style={styles.welcome}>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
         
       </View>
     )
@@ -35,13 +45,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'flex-end',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    fontSize: 10,
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA',
   },
 });
